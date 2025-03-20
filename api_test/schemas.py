5bb6017b997832be9bff2e31288e3266e2cc0776
dongwook <ddsntc1@naver.com>
2025-03-20 15:38:04 +0900

    Update schemas.py
    orm_mode 제거

--- api_test/schemas.py
+++ api_test/schemas.py
@@ -6,9 +6,6 @@ class ETFPrice(BaseModel):
     ticker : str
     price : float
     
-    class Config:
-        orm_mode = True
-    
     
 class BacktestRequest(BaseModel):
     start_year: int
@@ -18,33 +15,15 @@ class BacktestRequest(BaseModel):
     fee_rate: float
     weight_months: int
     
-    class Config:
-        orm_mode = True
     
 class Nav(BaseModel):
     data_id: int
     date: date
     nav_total: float
     
-    class Config:
-        orm_mode = True
     
 class RebalanceSchema(BaseModel):
     data_id: int
     date: date
     ticker: str
     weight: float
-
-    class Config:
-        orm_mode = True
-
-
-
-    # data_id = Column(Integer, primary_key=True,autoincrement=True, index=True)
-    # start_year = Column(Integer, nullable=False)
-    # start_month = Column(Integer,nullable=False)
-    # initial_balance = Column(Integer,nullable=False)
-    # trade_day = Column(Integer,nullable=False)
-    # fee_rate = Column(Numeric(5,4),nullable=False)
-    # weight_months = Column(Integer,nullable=False)
-    # created_at = Column(TIMESTAMP,server_default=func.now())
