27486b2268c3769f6fa4dcc8c601d17a0313dd31
dongwook <ddsntc1@naver.com>
2025-03-21 18:29:40 +0900

    test_v3
    데이터 프레임 수정 및 계산 편의를 위한 전처리 과정

--- api_test/services/backtest_crud.py
+++ api_test/services/backtest_crud.py
@@ -32,7 +32,7 @@ def get_prices(db:Session, start_date: date,weight_months: int):
 # 해결 방법 찾아야 함.
 
 # def calculate_backtest(db : Session, data_id: int, start_year: int, start_month:int, trade_day : int, initial_balance:int, weight_months:int,fee_rate:float):
-def calculate_backtest(db : Session, start_year: int, start_month:int, trade_day : int, weight_months:int):
+def calculate_backtest(db : Session, start_year: int, start_month:int, trade_day : int, weight_months:int,fee_rate:float):
     """ 계산 수행 """
     
     start_date = get_weekday(date(start_year,start_month,trade_day))
@@ -48,20 +48,68 @@ def calculate_backtest(db : Session, start_year: int, start_month:int, trade_day
         set_month -= weight_months
         set_year = start_year
     
+    # 거래 및 비교일 산출
     trade_dates = pd.date_range(start=pd.Timestamp(set_year,set_month,1), end=df_prices.index[-1],freq='MS')
     trade_dates = [get_weekday(date + timedelta(days=trade_day-1)) for date in trade_dates]
     
+    # 거래일에 해당하는 내용만 추출출
     df = df_prices.loc[trade_dates]
     
-    for col in df.columns:
+    # weight_months 이전 데이터와 합치기
+    raw_columns = df.columns
+    etfs = raw_columns.drop(['BIL','TIP'])
+
+    
+    for col in raw_columns:
         df[f'prev_{col}'] = df[col].shift(weight_months)
+    
+    # rebalance 계산
+    # df_cal : 계산을 위한 데이터프레임으로 시작일 부터의 데이터가 있음
+    # df : 시작일 - weight_months * 31 전부터 데이터가 존재하며 필요한 데이터 끌어올 용도
+    
+    df_cal = df.loc[start_date:].copy()
+    
+    # 리밸런싱 산정을 위한 wb_{col} 칼럼 생성 // weight_months 이전과 수익률 비교교
+    # -> 향후 1개월 전 가격과 비교한 수익률 칼럼으로 사용예정
+    for col in raw_columns:
+        df_cal[f'wb_{col}'] = (df_cal[col] - df_cal[f'prev_{col}'])/df_cal[f'prev_{col}']
+        if col != 'TIP':
+            df_cal[f'rb_{col}'] = float(0)
+    
+    # 리밸런스 비중 결정 -> rb_{col}로 0, 0.5, 1이 존재
+    for i, row in df_cal.iterrows():
+        tip_return = row['wb_TIP']
         
+        if tip_return >= 0:
+            selected = sorted(
+                [(etf,row[f'wb_{etf}']) for etf in etfs],
+                key= lambda x : x[1],
+                reverse= True)[:2]
+            for etf,_ in selected:
+                df_cal.loc[i, f'rb_{etf}'] = float(0.5)
+        else:
+            df_cal.loc[i, 'rb_BIL'] = float(1)
     
-    return df.loc[start_date:]
+    # wb_col 칼럼을 1개월 전과 비교하여 수익률 계산에 사용
+    # prev_col : 1개월 전 가격
+    # wb_col : 1개월 전과 비교한 수익률 칼럼
+    for col in raw_columns:
+        df_cal[f'prev_{col}'] = df[col].shift(1).loc[start_date:]
+        df_cal[f'wb_{col}'] = df_cal[col]/df_cal[f'prev_{col}']
+    
+    balances = raw_columns.drop('TIP')
+    
+    for col in balances:
+        df_cal[f'wb_{col}'][0] = 0
+    
     nav = 1000
-    peak_nav =nav
-    nav_history = []
-    rebalance_weights = []
+    nav_list = []
+    peak_nav = nav
+    drawdown_list = []
+    prev_weights = {col: 0 for col in raw_columns}
+
+
+    return df_cal
     
-    current_weights = {}
+
     
