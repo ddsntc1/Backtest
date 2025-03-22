d2b7366c0d27e76fe9cd2dbd9290328123329cd7
dongwook <ddsntc1@naver.com>
2025-03-23 00:44:13 +0900

    배치프로그램 수정
    상대경로 설정

--- update_prices.bat
+++ update_prices.bat
@@ -1,11 +1,7 @@
 @echo off
-pushd /d "C:\Users\ddsnt\OneDrive\Desktop\tripledouble\project"
+cd /d "%~dp0"  
 
 :: 가상환경 활성화
-call "C:\Users\ddsnt\OneDrive\Desktop\tripledouble\backtest\Scripts\activate.bat"
+call "..\backtest\Scripts\activate.bat"
 
-:: PYTHONPATH 환경 변수 설정 후 실행
-set PYTHONPATH=C:\Users\ddsnt\OneDrive\Desktop\tripledouble\project
 python update_prices.py
-
-pause
