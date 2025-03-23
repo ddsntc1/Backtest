dfccd179a1d325062c63e00e624bb9069af541e2
dongwook <ddsntc1@naver.com>
2025-03-23 16:57:30 +0900

    Update main.py

--- api_test/main.py
+++ api_test/main.py
@@ -1,12 +1,6 @@
-from fastapi import FastAPI,Depends
-from sqlalchemy.orm import Session
-from database import engine, get_db
+from fastapi import FastAPI
 from routes import backtest
 
 app = FastAPI(title = "트리플더블 과제테스트")
 
 app.include_router(backtest.router)
-
-@app.get("/")
-def read_root():
-    return {"message" : "안녕하세요. 지원자 강동욱 입니다."}
